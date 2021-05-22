202ec47fcb6f19b1e772fce303b52f38992b2171
AHMET YİĞİT ARITÜRK <l5217085@std.yildiz.edu.tr>
2021-05-23 00:48:13 +0300

    first commit

---
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -0,0 +1,13 @@
+package kodlamaio.northwind.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.entities.conretes.Product;
+
+public interface ProductService {
+
+	DataResult<List<Product>> getAll();
+	Result add(Product product);
+}
---
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,9 @@
+package kodlamaio.northwind.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.northwind.entities.conretes.Product;
+
+public interface ProductDao extends JpaRepository<Product, Integer> {
+
+}
